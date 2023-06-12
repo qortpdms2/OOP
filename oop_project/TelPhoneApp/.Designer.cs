f1b13feb432f5cb804a800b26344e8b4e03d0643
Seeun <128915671+qortpdms2@users.noreply.github.com>
2023-06-12 15:37:39 +0900

    feat) .Designer.cs

---
+++ oop_project/TelPhoneApp/.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace TelPhoneApp
+{
+    partial class sales_details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menubox = new System.Windows.Forms.TextBox();
+            this.pricebox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.countbox = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.sumbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.고유번호 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.이름 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.가격 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.개수 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.총가격 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.식별번호 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countbox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.고유번호,
+            this.이름,
+            this.가격,
+            this.개수,
+            this.총가격,
+            this.식별번호});
+            this.dataGridView2.Location = new System.Drawing.Point(12, 69);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(805, 373);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔스퀘어라운드OTF ExtraBold", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.BlueViolet;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Y&&R POS 판매 내역";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Regular", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(14, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(701, 19);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "                                                                                 " +
+    "                                                                                " +
+    "            ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Regular", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(842, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 19);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "물품검색";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menubox
+            // 
+            this.menubox.Location = new System.Drawing.Point(845, 96);
+            this.menubox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.menubox.Name = "menubox";
+            this.menubox.Size = new System.Drawing.Size(163, 25);
+            this.menubox.TabIndex = 25;
+            // 
+            // pricebox
+            // 
+            this.pricebox.Location = new System.Drawing.Point(22, 82);
+            this.pricebox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pricebox.Name = "pricebox";
+            this.pricebox.Size = new System.Drawing.Size(163, 25);
+            this.pricebox.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Regular", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(21, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 19);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "고유번호";
+            // 
+            // countbox
+            // 
+            this.countbox.Location = new System.Drawing.Point(23, 196);
+            this.countbox.Name = "countbox";
+            this.countbox.Size = new System.Drawing.Size(162, 25);
+            this.countbox.TabIndex = 19;
+            this.countbox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Regular", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(20, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 19);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "상품명";
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.Color.LightSlateGray;
+            this.Add.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Regular", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Add.Location = new System.Drawing.Point(36, 306);
+            this.Add.Margin = new System.Windows.Forms.Padding(0);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(71, 46);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "담기";
+            this.Add.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.pricebox);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.countbox);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.btnRemove);
+            this.panel1.Controls.Add(this.sumbox);
+            this.panel1.Controls.Add(this.Add);
+            this.panel1.Location = new System.Drawing.Point(823, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(212, 373);
+            this.panel1.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Regular", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(24, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 19);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "합계";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 258);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 25);
+            this.textBox1.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Regular", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label7.Location = new System.Drawing.Point(20, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 19);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "수량";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnRemove.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRemove.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Regular", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRemove.Location = new System.Drawing.Point(110, 306);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(74, 46);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "삭제";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            // 
+            // sumbox
+            // 
+            this.sumbox.Location = new System.Drawing.Point(22, 140);
+            this.sumbox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.sumbox.Name = "sumbox";
+            this.sumbox.Size = new System.Drawing.Size(163, 25);
+            this.sumbox.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Bold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(933, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 46);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 고유번호
+            // 
+            this.고유번호.HeaderText = "고유번호";
+            this.고유번호.MinimumWidth = 6;
+            this.고유번호.Name = "고유번호";
+            this.고유번호.Width = 125;
+            // 
+            // 이름
+            // 
+            this.이름.HeaderText = "이름";
+            this.이름.MinimumWidth = 6;
+            this.이름.Name = "이름";
+            this.이름.Width = 125;
+            // 
+            // 가격
+            // 
+            this.가격.HeaderText = "가격";
+            this.가격.MinimumWidth = 6;
+            this.가격.Name = "가격";
+            this.가격.Width = 125;
+            // 
+            // 개수
+            // 
+            this.개수.HeaderText = "개수";
+            this.개수.MinimumWidth = 6;
+            this.개수.Name = "개수";
+            this.개수.Width = 125;
+            // 
+            // 총가격
+            // 
+            this.총가격.HeaderText = "총가격";
+            this.총가격.MinimumWidth = 6;
+            this.총가격.Name = "총가격";
+            this.총가격.Width = 125;
+            // 
+            // 식별번호
+            // 
+            this.식별번호.HeaderText = "식별번호";
+            this.식별번호.MinimumWidth = 6;
+            this.식별번호.Name = "식별번호";
+            this.식별번호.Width = 125;
+            // 
+            // sales_details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1087, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menubox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label5);
+            this.Name = "sales_details";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "판매 내역";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countbox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox menubox;
+        private System.Windows.Forms.TextBox pricebox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown countbox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox sumbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 고유번호;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 이름;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 가격;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 개수;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 총가격;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 식별번호;
+    }
+}
